c4ecd83716f9e2674c2571800dc1eaa06a17e597
Guilherme Santos <111542791+GuilhermeSant0s@users.noreply.github.com>
2023-05-23 19:21:55 -0300

    Primeiro commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Importa a biblioteca random para gerar um número aleatório
+import random
+
+# Gera um número aleatório entre 1 e 100
+numero_aleatorio = random.randint(1, 100)
+
+# Inicializa a contagem de tentativas
+tentativas = 0
