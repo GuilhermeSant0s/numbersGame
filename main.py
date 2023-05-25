58b472df2f10e666a7b6c119bb657fb94e455a19
Guilherme Santos <111542791+GuilhermeSant0s@users.noreply.github.com>
2023-05-25 19:58:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,14 @@ numero_aleatorio = random.randint(1, 100)
 
 # Inicializa a contagem de tentativas
 tentativas = 0
+
+# Loop while para permitir que o usuário faça várias tentativas de adivinhar o número
+while True:
+    # Pede para o usuário digitar um número entre 1 e 100
+    palpite = int(input("Digite um número entre 1 e 100: "))
+    
+    # Verifica se o palpite do usuário é igual ao número aleatório gerado
+    if palpite == numero_aleatorio:
+        # Imprime uma mensagem de acerto e sai do loop
+        print("Parabéns! Você acertou!")
+        break
